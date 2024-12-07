1e0ae3c219ace175431849c2a29e6867f601624c
Kunal-JockL <kunalsalvak@gmail.com>
2024-12-07 21:44:28 +0530

    starting phase-2

--- classical-models/models.py
+++ classical-models/models.py
@@ -26,7 +26,7 @@ def logistic_regression_with_analysis(data, target_column, important_features, t
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
     # Compute class weights
-    class_weights = {0: len(y) / (2 * (y == 0).sum()), 1: len(y) / (2 * (y == 1).sum())}#change class weights formulae
+    class_weights = {0: len(y) / (2 * (y == 0).sum()), 1: len(y) / (2 * (y == 1).sum())} #change class weights formulae
 
     # Fit Logistic Regression model
     model = LogisticRegression(max_iter=1000, random_state=42, class_weight=class_weights)
