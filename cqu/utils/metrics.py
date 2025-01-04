7597d5ea7b12c1704acdc6b4fbd62fe44952fb56
Ian Tauro <sushruth127@gmail.com>
2025-01-05 01:18:15 +0530

    Updated classifier metrics to string to be more exhaustive

--- cqu/utils/metrics.py
+++ cqu/utils/metrics.py
@@ -11,32 +11,73 @@ import pandas as pd
 from sklearn.metrics import (
     accuracy_score,
     auc,
+    classification_report,
     confusion_matrix,
-    f1_score,
-    precision_score,
-    recall_score,
     roc_curve,
 )
+from tabulate import tabulate
 
 
 @dataclass
 class ClassifierMetrics:
+    report: str | dict
     accuracy: float
-    precision: float
-    recall: float
-    f1: float
     confusion_matrix: np.ndarray
     roc_c: tuple[np.ndarray, np.ndarray, np.ndarray]
     roc_auc: float
+    class_weights: dict | None
 
     def to_string(self) -> str:
+        classification_table_str = self.__get_classifcation_table_str()
+        summary_table_str = self.__get_summary_table_str()
+        confusion_matrix_str = self.__get_confusion_matrix_str()
+
         return (
-            f"Accuracy: {self.accuracy}\n"
-            f"Precision: {self.precision}\n"
-            f"Recall: {self.recall}\n"
-            f"F1: {self.f1}\n"
-            f"Confusion Matrix:\n{self.confusion_matrix}"
+            f"Classification Report:\n{classification_table_str}\n\n"
+            f"Summary:\n{summary_table_str}\n\n"
+            f"Confusion Matrix:\n{confusion_matrix_str}"
         )
+
+    def __get_classifcation_table_str(self) -> str:
+        classification_table = [
+            ["Class", "Precision", "Recall", "F1-Score", "Support"],
+        ]
+
+        for cls, metrics in classification_report.items():
+            if cls not in ["accuracy"]:
+                classification_table.append(
+                    [
+                        cls,
+                        metrics["precision"],
+                        metrics["recall"],
+                        metrics["f1-score"],
+                        metrics["support"],
+                    ]
+                )
+
+        classification_table_str = tabulate(
+            classification_table, headers="firstrow", tablefmt="grid"
+        )
+
+        return classification_table_str
+
+    def __get_summary_table_str(self) -> str:
+        summary_table = [
+            ["Metric", "Value"],
+            ["Accuracy", self.accuracy],
+            ["ROC AUC Score", self.roc_auc],
+            ["Threshold", self.roc_c[2]],
+        ]
+
+        if self.class_weights is not None:
+            summary_table.append(["Class Weights", self.class_weights])
+
+        summary_table_str = tabulate(summary_table, headers="firstrow", tablefmt="grid")
+
+        return summary_table_str
+
+    def __get_confusion_matrix_str(self) -> str:
+        return tabulate(self.confusion_matrix, tablefmt="grid")
 
     def __str__(self) -> str:
         return self.to_string()
@@ -48,13 +89,11 @@ class ClassifierMetrics:
 def get_metrics(
     y_true: np.ndarray | pd.Series, y_pred: np.ndarray | pd.Series
 ) -> ClassifierMetrics:
+    report = classification_report(y_true, y_pred, output_dict=True)
     accuracy = accuracy_score(y_true, y_pred)
-    precision = precision_score(y_true, y_pred)
-    recall = recall_score(y_true, y_pred)
-    f1 = f1_score(y_true, y_pred)
     cm = confusion_matrix(y_true, y_pred)
     fpr, tpr, thresholds = roc_curve(y_true, y_pred)
     roc_c = (fpr, tpr, thresholds)
     roc_auc = auc(fpr, tpr)
 
-    return ClassifierMetrics(accuracy, precision, recall, f1, cm, roc_c, roc_auc)
+    return ClassifierMetrics(accuracy, report, cm, roc_c, roc_auc)
